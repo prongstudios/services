6da234cc4157185a3deb1497dd769a9c47545368
Kevin Phelps <kphelps@engineyard.com>
2013-11-01 23:58:36 -0400

    added success parameter for drm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,16 @@ class PagesController < ApplicationController
 end
 
 class Drmstat
-	attr_reader :status
+	attr_reader :status, :success
 	def initialize
 		r = rand(100)
 		case r
 		when 0..50
 			@status = "DRM Failed! Your IP address has been logged.  Please contact support@tutorialthegame.com immediately!"
+			@success = false0
 		when 50..100
 			@status = "DRM Succeeded! Enjoy your registered copy of Tutorial: The Game"
+			@success = "false"
 		end
 	end
 end
