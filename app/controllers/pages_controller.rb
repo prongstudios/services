3e37f16a3cd8d10f11dac8a8beb8273b63a32113
Will Luongo <me@willluongo.com>
2013-11-01 22:34:06 -0400

    Render jsonified object for drm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
-  def drm
-  end
+	def drm
+		render json: Drmstat.new
+	end
 end
+
+class Drmstat
+	attr_reader :status
+	def initialize
+		r = rand(100)
+		case r
+		when 0..50
+			@status = "DRM Failed! Your IP address has been logged.  Please contact support@tutorialthegame.com immediately!"
+		when 50..100
+			@status = "DRM Succeeded! Enjoy your registered copy of Tutorial: The Game"
+		end
+	end
+end
+
+
